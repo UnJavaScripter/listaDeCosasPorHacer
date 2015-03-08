0feb509861b8ca2985011b5a0a75a7615d501ad7
Diego Coy <dcoy@equitel.com.co>
2015-03-08 12:26:59 -0500

    Creamos nuestro controlador

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-angular.module("ListaDeCosasPorHacer-App", [])
+angular
+	.module("ListaDeCosasPorHacer-App", [])
+		//Esta no es la mejor forma de escribir módulos adicionales, pero la usaremos por ahora para evitar confusiones con referencias a varios archivos
+		.controller("Controlador_Principal", function($scope){
+
+			//Una lista temporal que se va a ver en el navegador
+			$scope.listaDeCosas = ["Hacer diapositivas", "Comparar pan", "Pagar Internet"];
+
+		})
