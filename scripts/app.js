9a748257596eb0a21a7c4852a7eda5e985b43b81
Diego Coy <dcoy@equitel.com.co>
2015-03-08 12:31:07 -0500

    Añadimo un checkbox para mostrar el estado completado de cada tarea

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,20 @@ angular
 		.controller("Controlador_Principal", function($scope){
 
 			//Una lista temporal que se va a ver en el navegador
-			$scope.listaDeCosas = ["Hacer diapositivas", "Comparar pan", "Pagar Internet"];
+			//Para poder ver el estado completado la lista debe contener objetos en vez de strings simples.
+			//Cada nuevo objeto contiene un string con el nombre de la tarea (Cosa por hacer) y su estado: completado o no completado como valor booleano (true o false)
+			$scope.listaDeCosas = [
+				{ tarea: "Hacer diapositivas", completada: false },
+				{ tarea: "Comparar pan", completada: true },
+				{ tarea: "Pagar Internet", completada: false}
+			];
 
 			$scope.agregarCosa = function(){
 				//Si hay algo escrito en el campo de texto...
 				if($scope.cosaNueva.length > 0){
 					//Agregarlo a la lista de cosas y...
-					$scope.listaDeCosas.push($scope.cosaNueva);
+						//(Cada cosa nueva debe ser un objeto que sigue el patrón de $scope.listaDeCosa, debe tener las propiedades tarea  y completada)
+					$scope.listaDeCosas.push({tarea: $scope.cosaNueva, completada: false});
 				}
 				//Borrar el contenido del campo de texto
 				$scope.cosaNueva = "";
