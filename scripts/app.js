a17a20ebdc996bb2bf49d53ec9acc765667ee1b2
Diego Coy <dcoy@equitel.com.co>
2015-03-08 12:29:41 -0500

    Creamos un formulario para agregar elementos a la lista

--- scripts/app.js
+++ scripts/app.js
@@ -6,4 +6,14 @@ angular
 			//Una lista temporal que se va a ver en el navegador
 			$scope.listaDeCosas = ["Hacer diapositivas", "Comparar pan", "Pagar Internet"];
 
+			$scope.agregarCosa = function(){
+				//Si hay algo escrito en el campo de texto...
+				if($scope.cosaNueva.length > 0){
+					//Agregarlo a la lista de cosas y...
+					$scope.listaDeCosas.push($scope.cosaNueva);
+				}
+				//Borrar el contenido del campo de texto
+				$scope.cosaNueva = "";
+			};
+
 		})
