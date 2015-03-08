de99a9b5033db31f9f535f1ee13de72e274e081a
Diego Coy <dcoy@equitel.com.co>
2015-03-08 12:38:32 -0500

    Usamos filtros predefinidos por AngularJS como filtros para strings y un fíltro aplicado a un ng-repeat. También creamos nuestro propio filtro

--- scripts/app.js
+++ scripts/app.js
@@ -24,3 +24,17 @@ angular
 			};
 
 		})
+		//Filtro personalizado
+			// https://docs.angularjs.org/guide/filter
+		.filter('sinCompletar', function(){
+			return function(input){
+				var sinCompletar = 0;
+				input.forEach(function(elemento){
+					if(elemento.completada === false){
+						sinCompletar += 1;
+					}
+				})
+
+				return sinCompletar;
+			};
+		})
